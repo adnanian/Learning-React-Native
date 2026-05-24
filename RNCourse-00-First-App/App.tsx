8fabd96ceaebaa28e89df332379201ca02a3d2a6
Adnan Wazwaz <73372148+adnanian@users.noreply.github.com>
2026-05-24 00:22:49 -0500

    Recreate first learning project

--- RNCourse-00-First-App/App.tsx
+++ RNCourse-00-First-App/App.tsx
@@ -1,12 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View>
-        <Text>Another piece of text.</Text>
-      </View>
-      <Text>Hello, world!</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
   );
 }
